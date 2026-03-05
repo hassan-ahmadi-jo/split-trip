8507514ca11254b1dcf3770385abe1313eaca77f
Hassan <ha522562@gmail.com>
2026-03-05 15:47:23 +0330

    feat(accounts): create accounts app
    - Create custom User model with email activation
    - Use email address as the username field
    - Configure AUTH_USER_MODEL in settings

--- SplitTrip/settings.py
+++ SplitTrip/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+
+# User
+
+AUTH_USER_MODEL = 'accounts.User'
---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import random
+
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email address')
+    is_email_active = models.BooleanField(default=False, verbose_name='Is email active')
+    email_active_code = models.CharField(max_length=6, verbose_name='Email active code')
+
+    def save(self, *args, **kwargs):
+        self.username = self.email
+        return super().save(*args, **kwargs)
