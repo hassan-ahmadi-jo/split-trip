330710a02297faa62429c224f4e102dd9bf3fcb7
Hassan <ha522562@gmail.com>
2026-05-06 11:54:50 +0330

    feat(events): add admin interface for all event models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(models.EventMembership)
+class EventMembershipAdmin(admin.ModelAdmin):
+    list_display = ['event_title', 'user_first_name', 'user_email']
+
+    def event_title(self, obj):
+        return obj.event.title
+
+    def user_first_name(self, obj):
+        return obj.user.first_name
+
+    def user_email(self, obj):
+        return obj.user.email
+
+@admin.register(models.EventJoinRequest)
+class EventMembershipAdmin(admin.ModelAdmin):
+    list_display = ['event_title', 'user_first_name', 'user_email']
+
+    def event_title(self, obj):
+        return obj.event.title
+
+    def user_first_name(self, obj):
+        return obj.user.first_name
+
+    def user_email(self, obj):
+        return obj.user.email
