383b1b0f4246369aae333ca662f52f58c894222f
Hassan <ha522562@gmail.com>
2026-05-06 11:13:37 +0330

    feat(accounts): add admin interface for User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'email', 'is_email_active', 'email_active_code']
+    list_filter = ['is_email_active']
+    list_editable = ['is_email_active']
+    list_per_page = 20
