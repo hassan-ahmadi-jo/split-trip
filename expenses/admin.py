68ad72a53bddc0aada7dad12648b0628b2656fac
Hassan <ha522562@gmail.com>
2026-05-08 22:54:48 +0330

    feat(expenses): add admin interface for expenses models

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Participants)
+class ParticipantsAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'event__title', 'total_paid', 'total_share']
+    list_filter = ['full_name', 'event__title']
+
+    
+@admin.register(models.Expenses)
+class moduleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'event__title', 'total_amount']
+    list_filter = ['event__title']
+
+
+@admin.register(models.ParticipantExpense)
+class ParticipantExpenseAdmin(admin.ModelAdmin):
+    list_display = ['participant__full_name', 'expense__title', 'paid_amount', 'share_amount']
+    list_filter = ['participant__full_name', 'expense__title']
